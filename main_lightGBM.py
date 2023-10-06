d99bd2955ac60fcbd960a52d76d7200bf03a2ec7
TakubonS <son-takubon@g.ecc.u-tokyo.ac.jp>
2023-10-06 12:36:47 +0900

    base

--- main_lightGBM.py
+++ main_lightGBM.py
@@ -24,9 +24,15 @@ if __name__ == '__main__':
 
     X = train_df.drop(['Survived'], axis=1)
     X = convert_obj_to_category(X)
+    categorical_features = X.select_dtypes(include=['category']).columns.to_list()
     y = train_df['Survived']
     X_test = convert_obj_to_category(test_df)
 
     # split train_df into train and valid with train_test_split
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)
-    print(X_train.shape())
+
+    cls = lgb.LGBMClassifier()
+    cls.fit(X_train, y_train, categorical_feature = categorical_features)
+    y_pred = cls.predict(X_valid)
+    # print accuracy percentage
+    print('Accuracy: {:.2f}%'.format(cls.score(X_valid, y_valid) * 100))
